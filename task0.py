78b876d2900bf726db617bb2c85b5995657ead98
sai <sh354053@dal.ca>
2019-05-23 05:53:10 -0700

    add first commit

---
+++ task0.py
@@ -0,0 +1,12 @@
+import math
+
+for i in range(1,100):
+    if i%3==0:
+      print(f"the number {i} is fizz")
+    elif i%5==0:
+      print("Buzz")
+    else:
+     if(i%3==0 and i%5==0):
+      print("FizzBuzz")
+
+Print(i)
